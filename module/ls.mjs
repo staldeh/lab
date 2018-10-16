88808f6057527d21d5ecfec0728d65c83a6626db
Stefan Albinsson <stefan.albinsson@gmail.com>
2018-10-16 13:23:37 +0200

    Modules

---
+++ module/ls.mjs
@@ -0,0 +1,11 @@
+import _fs from 'fs';
+const fs = _fs.promises;
+import util from 'util';
+(async () => {
+  const files = await fs.readdir('.');
+  for (let fn of files) {
+    console.log(fn);
+  }
+})().catch(err => {
+  console.error(err);
+});
---
+++ module/showdata.js
@@ -0,0 +1,3 @@
+const util = require('util');
+const data = require('./data');
+console.log(util.inspect(data));
