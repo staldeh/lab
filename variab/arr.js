dcb7fc48e25c91165cbd888e5671d00c7d5da617
Albinsson <stefan.albinsson@gmail.com>
2018-12-09 09:45:40 +0100

    arr

--- variab/arr.js
+++ variab/arr.js
@@ -9,8 +9,22 @@ ph["a"] = 'Ojoj';
 ph["b"] = 'hoj';
 console.log (ph);
 
+console.log('sen:')
+
+var s = {"d" :  {"a" : 4, "b" : 3} }
+console.log(s.d)
 var sen = JSON.parse('{"d" :  {"a" : 4, "b" : 3} }')
+console.log(sen)
+console.log(JSON.stringify(sen))
 
 console.log(sen["d"])
 console.log(sen["d"]["a"])
 console.log(sen.d.b)
+console.log('Arrays:')
+
+let arr = [];
+
+arr["data"] = '{ "d" { "temp" : 4 }}'
+arr["data"] = '{ "d" { "temp" : 1 }}'
+arr.push ( '{ "d" { "temp" : 5 }}')
+console.log(arr)
