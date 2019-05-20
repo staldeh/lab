c7f79072430e289318b76484d93cd19cabf48dc7
staldeh <stefan.albinsson@gmail.com>
2019-05-20 22:26:04 +0200

    Jontish

--- game/go.js
+++ game/go.js
@@ -1,17 +1,21 @@
 const MapS = require('./map.js')
 const objS = require('./object.js')
 
-const Indivudals = {M: new objS("Monster"),
+const Indivudals = {H: new objS("Häxa"),
                     D: new objS("DU"),
-                    S: new objS("Skatt")}
+                    S: new objS("Skatt"),
+                    K: new objS("Katana"),
+                    C: new objS("Chainmail")}
 
 // World Size
 const world = new MapS(5)
 
 //Some objects
-world.poke ([0,2], Indivudals.M)
-world.poke ([4,4], Indivudals.D)
+world.poke ([0,2], Indivudals.H)
+world.poke ([4,3], Indivudals.D)
 world.poke ([1,1], Indivudals.S)
+world.poke ([2,3], Indivudals.K)
+world.poke ([3,4], Indivudals.C)
 
 //console.log(world.list())
 //console.log(Indivudals)
--- game/object.js
+++ game/object.js
@@ -5,6 +5,7 @@ class ObjS {
 
     name () { return this._name}
     behave () {}
+    equip () {}
 }
 
 module.exports = ObjS;
