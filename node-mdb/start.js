ce3104f3f663989a3fa56efe133c90d0fc0fb8be
Stefan Albinsson <stefan.albinsson@gmail.com>
2019-09-30 16:19:50 +0200

    mongodb

---
+++ node-mdb/start.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.DATABASE) ;
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .on('connected', () => {
+    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+
+require('./models/Registration');
+const app = require('./app');
+const server = app.listen(3000, () => {
+  console.log(`Express is running on port ${server.address().port}`);
+});
